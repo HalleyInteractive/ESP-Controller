68d46870dfc8bed513d98b5d1261d492c98a16d9
Niels Overwijn <nielsoverwijn@google.com>
2022-04-14 21:36:52 +0200

    PortController connect

---
+++ src/serial/port-controller.ts
@@ -0,0 +1,19 @@
+export class PortController {
+  private serialOptions: SerialOptions = {
+    baudRate: 115200,
+    dataBits: 8,
+    stopBits: 1,
+    bufferSize: 255,
+    parity: 'none',
+    flowControl: 'none',
+  };
+
+  connected: Boolean = false;
+
+  constructor(private readonly port: SerialPort) {}
+
+  async connect() {
+    await this.port.open(this.serialOptions);
+    this.connected = true;
+  }
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,17 @@
-describe('calculate', () => {
-  it('add', () => {
-    const result = 2 + 5;
-    expect(result).toBe(7);
+import {PortController} from './../src/serial/port-controller';
+
+describe('Connect port on PortController', () => {
+  it('should call open on port', async () => {
+    const mockPort = {
+      open: jest.fn(),
+    };
+    const portController: PortController = new PortController(mockPort);
+
+    expect(portController.connected).toBe(false);
+
+    await portController.connect();
+
+    expect(mockPort.open.mock.calls.length).toBe(1);
+    expect(portController.connected).toBe(true);
   });
 });
