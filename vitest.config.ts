fcd156e3b61b5558948266c28c9b992cd251f1c5
Niels Overwijn <niels.overwijn@gmail.com>
2025-06-08 22:12:21 +0200

    Set up coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,17 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    coverage: {
+      // Use the 'v8' provider for coverage
+      provider: "v8",
+      // Specify which files to include in the coverage report
+      include: ["src/**/*.ts"],
+      // Specify which files to exclude from the coverage report
+      exclude: ["src/**/*.test.ts", "src/index.ts"],
+      // Specify the reporters to use
+      reporter: ["text", "json", "html"],
+      // Generate a detailed HTML report in the 'coverage' directory
+      reportsDirectory: "./coverage",
+    },
   },
 });
